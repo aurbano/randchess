7deabe6961caf2fbc6f169af293e8ba2b4b28c08
Alejandro U. Alvarez <aluralma@gmail.com>
2023-01-04 14:49:31 +0000

    allow pawns to move 2 when in their starting location

--- src/components/Board/pieces/pawn.ts
+++ src/components/Board/pieces/pawn.ts
@@ -4,6 +4,12 @@ export const pawn = (identity: -1 | 1): Piece => ({
   identity,
   label: 'P',
   move: (location: Coordinates) => {
+    if (location.y == 1 && identity == -1 || location.y == 6 && identity == 1) {
+      return [
+        {x: location.x, y: location.y - identity * 1},
+        {x: location.x, y: location.y - identity * 2},
+      ];
+    }
     return [
       {x: location.x, y: location.y - identity * 1},
     ];
